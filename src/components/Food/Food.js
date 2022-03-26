ece1b7282d2db3f4f35c229481a24f6618a08344
programmer-jafir <jafiranjum2020@gmail.com>
2022-03-26 18:39:52 +0600

    Add to cart icon added

--- src/components/Food/Food.js
+++ src/components/Food/Food.js
@@ -1,3 +1,5 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 import React from 'react';
 import './Food.css'
 
@@ -14,7 +16,8 @@ const Food = (props) => {
 
             </div>
             <button onClick={() => handleAddToCart(food)} className='btn-cart'>
-                <p>Add to Cart</p>
+                <p className='btn-text'>Add to Cart</p>
+                <FontAwesomeIcon icon={faShoppingCart}></FontAwesomeIcon>
             </button>
         </div>
     );
